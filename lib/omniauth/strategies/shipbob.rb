5575ee6cef169fc5c474e4d120f74923e0679a1a
Kishore <kishore.venkat@getdropstream.com>
2019-12-06 21:59:18 +0530

    modify shipbob strategy and tested by passing authorize params and implemented get_channel_id API

--- lib/omniauth/strategies/shipbob.rb
+++ lib/omniauth/strategies/shipbob.rb
@@ -1,4 +1,5 @@
 require 'omniauth-oauth2'
+require 'json'
 
 module OmniAuth
   module Strategies
@@ -10,11 +11,21 @@ module OmniAuth
         :site => 'https://auth.shipbob.com'
       }
       
-      #option :provider_ignores_state, true
+      credentials do
+        hash = {"token" => access_token.token}
+        hash["refresh_token"] = access_token.refresh_token if access_token.expires? && access_token.refresh_token
+        hash["expires_at"] = access_token.expires_at if access_token.expires?
+        hash["expires"] = access_token.expires?
+        hash['channel_id'] = get_channel_id(access_token)
+        hash
+      end
       
-      def callback_phase
-        binding.pry
-        super
+      def get_channel_id(token)
+        log :info, 'Calling API to get Channel Id.'
+        response = token.get('https://api.shipbob.com/1.0/channel', :headers => { 'Content-Type' => 'application/json' })
+        JSON.parse(response.body).dig(0, "id")
+      rescue => e
+        nil  
       end
       
     end
